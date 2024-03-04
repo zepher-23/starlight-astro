8842dd351701f59f1ee214ba1838c889905e90b8
zepher-23 <30zepher@gmail.com>
2024-03-04 23:33:36 +0530

    added home route

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,43 @@
-import React,{useEffect} from "react";
+import React,{useRef} from "react";
 import '../global.css'
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+    const searchRef = useRef(null)
+    const resultRef = useRef(null)
+    console.log(searchRef)
+    const data = props.data
+    
+
+    const search =() =>{
+        // console.log("clicked")
+        let value = searchRef.current.value 
+
+    }
+
+    const display =()=>{
+        if(resultRef.current.style.display === 'flex'){
+            resultRef.current.style.display = 'none'
+        }else{
+            resultRef.current.style.display = 'flex'
+        }
+
+
+    }
+    
    
 
   return(
-    <div style={{width:'100vw',display:'flex',justifyContent:'center', padding:'20px'}}>
-    <input type="text" placeholder="Search" name="search" className="search" />
-    <button style={{cursor:'pointer'}}>Search</button>
-    
-    </div>
+    <>
+    <div style={{ width: '100vw', display: 'flex', justifyContent: 'center', position: 'relative',margin:'20px' }}>
+                <input type="text" placeholder="Search" onBlur={display} onFocus={display} ref={searchRef} name="search" className="search" />
+                <button style={{ cursor: 'pointer' }} onClick={search}>Search</button>
+                <div className="searchResults" ref={resultRef} style={{display:'none', backgroundColor: 'white',width:'100%',maxWidth:'100%', height: '300px', position: 'absolute', top: '110%',left:0 ,zIndex:999,borderRadius:'10px'}}>
+                    <div style={{padding:'20px'}}>
+
+                    </div>
+                </div>
+            </div>
+            </>
 
   );
 };
