f796393ed80edfb0056b8fe1b97c62fff5b3d508
zepher-23 <30zepher@gmail.com>
2024-03-04 21:10:55 +0530

    fixed minor bugs

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React,{useEffect} from "react";
 import '../global.css'
 
 const SearchBar = () => {
+   
 
   return(
     <div style={{width:'100vw',display:'flex',justifyContent:'center', padding:'20px'}}>
-    <input type="text" placeholder="Search" style={{width:"100%",height:'40px'}}/>
-    <button style={{width:'100px',cursor:'pointer'}}>Submit</button>
+    <input type="text" placeholder="Search" name="search" className="search" />
+    <button style={{cursor:'pointer'}}>button</button>
+    
     </div>
+
   );
 };
 
