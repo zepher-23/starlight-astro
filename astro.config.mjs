59833647af1e6b556976d81231d6bd49437cfb9b
zepher-23 <30zepher@gmail.com>
2024-03-09 13:05:38 +0530

    sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()]
+  prefetch: {
+    prefetchAll: true
+  },
+  site: 'https://starlightastro.netlify.app',
+  integrations: [react(), mdx(), sitemap(), robotsTxt()]
 });
